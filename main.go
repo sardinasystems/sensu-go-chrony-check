4cf6b1971fed2d8e4c52c2d55286364bab87d792
Vladimir Ermakov <vooon341@gmail.com>
2021-01-11 18:23:24 +0300

    implement main set of checks

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"path"
@@ -18,9 +19,16 @@ import (
 type Config struct {
 	sensu.PluginConfig
 	SocketPath string
-	// Jitter? Stratum?
-	OffsetWarning  float64
-	OffsetCritical float64
+	// Jitter?
+	StratumWarning  uint
+	StratumCritical uint
+	//OffsetWarning  float64
+	//OffsetCritical float64
+	LastRxWarning        uint
+	LastRxCritical       uint
+	ReachabilityWarning  float64
+	ReachabilityCritical float64
+	Debug                bool
 }
 
 var (
@@ -43,22 +51,85 @@ var (
 			Value:     &plugin.SocketPath,
 		},
 		{
-			Path:      "offset_warning",
-			Env:       "CHRONY_OFFSET_WARNING",
-			Argument:  "offset-warning",
-			Shorthand: "w",
-			Default:   0.050,
-			Usage:     "Offset warning level [s]",
-			Value:     &plugin.OffsetWarning,
+			Path:     "stratum_warning",
+			Env:      "CHRONY_STRATUM_WARNING",
+			Argument: "stratum-warning",
+			//Shorthand: "sw",
+			Default: uint(10),
+			Usage:   "Stratum warning level",
+			Value:   &plugin.StratumWarning,
 		},
 		{
-			Path:      "offset_critical",
-			Env:       "CHRONY_OFFSET_CRITICAL",
-			Argument:  "offset-critical",
+			Path:     "stratum_critical",
+			Env:      "CHRONY_STRATUM_CRITICAL",
+			Argument: "stratum-critical",
+			//Shorthand: "sc",
+			Default: uint(12),
+			Usage:   "Stratum critical level",
+			Value:   &plugin.StratumCritical,
+		},
+		// {
+		// 	Path:      "offset_warning",
+		// 	Env:       "CHRONY_OFFSET_WARNING",
+		// 	Argument:  "offset-warning",
+		// 	Shorthand: "ow",
+		// 	Default:   0.050,
+		// 	Usage:     "Offset warning level [s]",
+		// 	Value:     &plugin.OffsetWarning,
+		// },
+		// {
+		// 	Path:      "offset_critical",
+		// 	Env:       "CHRONY_OFFSET_CRITICAL",
+		// 	Argument:  "offset-critical",
+		// 	Shorthand: "oc",
+		// 	Default:   0.100,
+		// 	Usage:     "Offset critical level [s]",
+		// 	Value:     &plugin.OffsetCritical,
+		// },
+		{
+			Path:      "lastrx_warning",
+			Env:       "CHRONY_LASTRX_WARNING",
+			Argument:  "lastrx-warning",
+			Shorthand: "W",
+			Default:   uint(64),
+			Usage:     "LastRx warning level [s]",
+			Value:     &plugin.LastRxWarning,
+		},
+		{
+			Path:      "lastrx_critical",
+			Env:       "CHRONY_LASTRX_CRITICAL",
+			Argument:  "lastrx-critical",
+			Shorthand: "C",
+			Default:   uint(128),
+			Usage:     "LastRx critical level [s]",
+			Value:     &plugin.LastRxCritical,
+		},
+		{
+			Path:      "reachability_warning",
+			Env:       "CHRONY_REACHABILITY_WARNING",
+			Argument:  "reachablility-warning",
+			Shorthand: "w",
+			Default:   67.0,
+			Usage:     "Reachablility warning percent",
+			Value:     &plugin.ReachabilityWarning,
+		},
+		{
+			Path:      "reachability_critical",
+			Env:       "CHRONY_REACHABILITY_CRITICAL",
+			Argument:  "reachablility-critical",
 			Shorthand: "c",
-			Default:   0.100,
-			Usage:     "Offset critical level [s]",
-			Value:     &plugin.OffsetCritical,
+			Default:   34.0,
+			Usage:     "Reachablility critical percent",
+			Value:     &plugin.ReachabilityCritical,
+		},
+		{
+			Path:      "debug",
+			Env:       "CHRONY_DEBUG",
+			Argument:  "debug",
+			Shorthand: "d",
+			Default:   false,
+			Usage:     "output debugging data",
+			Value:     &plugin.Debug,
 		},
 	}
 )
@@ -91,12 +162,70 @@ func executeCheck(event *types.Event) (int, error) {
 		return sensu.CheckStateUnknown, err
 	}
 
-	_ = stats
+	// Print debug data on exit
+	defer func() {
+		if plugin.Debug {
+			b, _ := json.MarshalIndent(stats, "", "  ")
+			fmt.Println(string(b))
+		}
+	}()
 
-	b, _ := json.MarshalIndent(stats, "", "  ")
-	fmt.Println(string(b))
+	if stats.Tracking.IPAddr.IsUnspecified() {
+		log.Printf("No tracking source!")
+		return sensu.CheckStateCritical, nil
+	}
 
-	return sensu.CheckStateOK, nil
+	log.Printf("Tracking server: %08X (%v)", stats.Tracking.RefID, stats.Tracking.IPAddr)
+	if uint(stats.Tracking.Stratum) >= plugin.StratumCritical {
+		log.Printf("CRITICAL: Tracking server stratum: %d", stats.Tracking.Stratum)
+		return sensu.CheckStateCritical, nil
+	} else if uint(stats.Tracking.Stratum) >= plugin.StratumWarning {
+		log.Printf("WARNING: Tracking server stratum: %d", stats.Tracking.Stratum)
+		return sensu.CheckStateWarning, nil
+	}
+
+	result := sensu.CheckStateOK
+	setResult := func(newResult int) {
+		if newResult > result {
+			result = newResult
+		}
+	}
+
+	reachability := 0.0
+	for _, source := range stats.Sources {
+		if source.Reachability == 255 {
+			reachability += 100.0
+		}
+
+		// skip unreachable sources
+		if source.State == chrony.SourceStateUnreach {
+			continue
+		}
+
+		lastRx := uint(source.SinceSample)
+		if lastRx >= plugin.LastRxCritical {
+			log.Printf("CRITICAL: %v server LastRx: %d", source.IPAddr, lastRx)
+			setResult(sensu.CheckStateCritical)
+		} else if lastRx >= plugin.LastRxWarning {
+			log.Printf("WARNING: %v server LastRx: %d", source.IPAddr, lastRx)
+			setResult(sensu.CheckStateWarning)
+		}
+	}
+
+	reachability /= float64(len(stats.Sources))
+	if reachability <= plugin.ReachabilityCritical {
+		log.Printf("CRITICAL: only %.1f%% sources reachable!", reachability)
+		setResult(sensu.CheckStateCritical)
+	} else if reachability <= plugin.ReachabilityWarning {
+		log.Printf("WARNING: only %.1f%% sources reachable!", reachability)
+		setResult(sensu.CheckStateWarning)
+	}
+
+	if result == sensu.CheckStateOK {
+		log.Printf("Chrony status: OK")
+	}
+
+	return result, nil
 }
 
 type stats struct {
